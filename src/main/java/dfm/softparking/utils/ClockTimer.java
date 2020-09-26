44674200be8acce53c39e8d6fb14d9f8d16769b9
Daniel Morales <danielf.moralesp20@gmail.com>
2020-09-25 21:54:48 -0500

    feat: clock classes
    A clock classes for format and timer

---
+++ src/main/java/dfm/softparking/utils/ClockTimer.java
@@ -0,0 +1,52 @@
+package dfm.softparking.utils;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+public class ClockTimer implements Runnable {
+	private transient PropertyChangeSupport changeListeners;
+	private final long period = 1;
+	private final TimeUnit timeUnit = TimeUnit.SECONDS;
+	
+	private ScheduledExecutorService scheduledExcServ;
+	private ScheduledFuture<?> scheduledFuture;
+	private Date date;
+	
+	public ClockTimer() {
+		changeListeners = new PropertyChangeSupport(this);
+		scheduledExcServ = Executors.newSingleThreadScheduledExecutor();
+	}
+	
+	@Override
+	public void run() {
+		Date oldValue = date;
+		date = new Date();
+		changeListeners.firePropertyChange(this.getClass().getSimpleName(), oldValue, date);
+	}
+	
+	public void startClock() {
+		stopClock();
+		scheduledFuture = scheduledExcServ.scheduleAtFixedRate(this, 0, period, timeUnit);
+	}
+	
+	public void stopClock() {
+		if(scheduledFuture != null)
+			scheduledFuture.cancel(false);
+	}
+	
+	
+	public synchronized void addPropertyChangeListener(PropertyChangeListener listener) {
+    	changeListeners.addPropertyChangeListener(listener);
+    }
+
+    public synchronized void removePropertyChangeListener(PropertyChangeListener listener) {
+    	changeListeners.removePropertyChangeListener(listener);
+    }
+}
+
+    
